ba3299f0e009ecc55badc5d5eac8fc9372215744
Andrea <andreaincannila@gmail.com>
2025-07-25 07:49:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-
-const CACHE_NAME = 'guida-pergola-cache-v1';
-const FILES_TO_CACHE = [
+// sw.js
+const CACHE_NAME = 'mp-tende-cache-v1';
+const URLS_TO_CACHE = [
   '/',
   '/index.html',
   '/style.css',
@@ -8,32 +8,14 @@ const FILES_TO_CACHE = [
   '/manifest.json'
 ];
 
-// Install
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
-  self.skipWaiting();
 });
 
-// Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    })
-  );
-  self.clients.claim();
-});
-
-// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
